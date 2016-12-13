c18befd17df90ec618ca72dd71c9514721503cb3
Hassan <hassan.reyes@gmail.com>
2016-12-13 05:14:05 +0000

    file size handling

--- server.js
+++ server.js
@@ -5,18 +5,25 @@ var express = require('express'),
     
     
 var storage = multer.memoryStorage();
-var upload = multer({ storage: storage });
+var upload = multer({ storage: storage, limits: { fileSize: 1000000 } }).single('uploadedFile');
 
-app.post('/upload', upload.single('uploadedFile'), function (req, res, next) {
-    res.json({ size: req.file.size });
-    next();
+app.post('/upload', function (req, res, next) {
+    upload(req, res, (err) => {
+        if(err){
+            console.error('An error occured', err);
+            return res.json({ error: err.message });
+        }
+        
+        res.json({ size: req.file.size });
+        next();
+    });
 })
     
 /* Home Page */
 app.get('/', (req, res) => {
     var url = req.protocol + "://" + req.hostname + "/api/";
     var html = "<h2>File Metadata Microservice</h2>"
-            +   "<p>Select a file to upload:</p>"
+            +   "<p>Select a file to upload (1MB Max):</p>"
             +   "<form method='POST' action='/upload' enctype='multipart/form-data'>"
             +       "<input type='file' name='uploadedFile'>"
             +       "<INPUT type='submit'>"
